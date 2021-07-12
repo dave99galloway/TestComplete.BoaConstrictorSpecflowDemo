4e670c9e943c0e29090eae67750d38c3953f88fc
Dave <Dave@DESKTOP-8MCCCOA>
2021-07-12 16:27:09 +0100

    master - added Boa.Constrictor dependency, instantiated webdriver from scoped hook binding (hard coded to local chromedriver), removed auto retries for failed tests

---
+++ TestComplete.BoaConstrictorSpecflowDemo/Hooks/WebUiHooks.cs
@@ -0,0 +1,42 @@
+﻿using BoDi;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace TestComplete.BoaConstrictorSpecflowDemo.Hooks
+{
+    [Binding, Scope(Tag = "WebUi")]
+    public sealed class WebUiHooks
+    {
+        private readonly IObjectContainer _objectContainer;
+
+        public WebUiHooks(IObjectContainer objectContainer)
+        {
+            this._objectContainer = objectContainer;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            _objectContainer.RegisterFactoryAs(c => WebDriverFactory());
+        }
+
+        private IWebDriver WebDriverFactory()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            return new ChromeDriver();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            _objectContainer.Resolve<IWebDriver>().Quit();
+        }
+    }
+}
